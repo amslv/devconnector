ce9a1d12e137731101316baac60ed72ff564590d
Ana Silva <as.anasilva@outlook.com>
2018-07-14 15:58:24 -0300

    mongodb connected

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://ana:admin123@ds239071.mlab.com:39071/dev-connector-db"
+};
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// db config
+
+const db = require("./config/keys").mongoURI;
+
+// connects to mongo db
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongoose connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("It works"));
 
