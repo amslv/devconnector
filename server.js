7ee8726d185770205ee4425bbba8387169c547d0
Ana Silva <as.anasilva@outlook.com>
2018-07-14 16:19:49 -0300

    routes update

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const post = require("./routes/api/post");
 
 const app = express();
 
@@ -15,6 +18,12 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("It works"));
+
+// use routes
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", post);
 
 const port = process.env.PORT || 5000;
 
